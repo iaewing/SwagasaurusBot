008fb174866ed246a903e3d30896317c7adb732b
Ian <iewing085@gmail.com>
2021-01-12 17:21:47 -0500

    Added more commands. Super hacky right now

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 //Add my discord.js dependency
 const Discord = require('discord.js');
-
+//Adds the configuration JSON as a dependency
+const { prefix, token } = require('./config.json');
 //create my client
 const client = new Discord.Client();
-
-//For easy removal before posting somewhere
-const token = "Nzk4NjYyMzIzMTE1Nzg2Mjgw.X_4SQA.9nUYIW0gsS8yjD1aHmlR1UbdExc";
-
-
-client.on('message', message => {
-	if (message.content === '!magic') {
-    message.channel.send('Get Rekt Cam');
-  }
-});
 
 //Run this once at ready
 client.once('ready', () => {
   console.log('Ready');
+});
+
+client.on('message', message => {
+	if (message.content === `${prefix}magic`) {
+    message.channel.send(`Get Rekt ${message.author.username}`);
+  } else if (message.content ===`${prefix}beep`) {
+    message.channel.send('Boop');
+  } else if (message.content === `${prefix}server`) {
+    message.channel.send(`This server's name is: ${message.guild.name}\n
+    Total members: ${message.guild.memberCount}`);
+  } else if (message.content === `${prefix}user-info`) {
+    message.channel.send(`Your username: ${message.author.username}\n
+      Your ID: ${message.author.id}`);
+  }
 });
 
 //Log the bot into Discord using the token
