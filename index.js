2dacfe46d65b679691cce16f744c6b85bf64ee72
Ian <iewing085@gmail.com>
2021-01-12 20:25:09 -0500

    began to move commands into separate files

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
 //Add my discord.js dependency
 const Discord = require('discord.js');
 //Adds the configuration JSON as a dependency
 const { prefix, token } = require('./config.json');
 //create my client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 //Run this once at ready
 client.once('ready', () => {
@@ -11,16 +14,32 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === `${prefix}magic`) {
-    message.channel.send(`Get Rekt ${message.author.username}`);
-  } else if (message.content ===`${prefix}beep`) {
+  //Check for prefix and to make sure its not from a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) {
+    return;
+  }
+
+  //Grab just the args into an array by slicing the prefix number of characters
+  //and trimming whitepsace
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  //Grab the first arg and lowercase it
+  const command = args.shift().toLowerCase();
+
+	if (command === 'magic') {
+    message.channel.send(`Get Rekt ${message.author}`);
+  } else if (command === 'beep') {
     message.channel.send('Boop');
-  } else if (message.content === `${prefix}server`) {
+  } else if (command === 'server') {
     message.channel.send(`This server's name is: ${message.guild.name}\n
     Total members: ${message.guild.memberCount}`);
-  } else if (message.content === `${prefix}user-info`) {
+  } else if (command === 'user-info') {
     message.channel.send(`Your username: ${message.author.username}\n
       Your ID: ${message.author.id}`);
+  } else if (command === 'args-info') {
+    if (!args.length) {
+      return message.reply(', you did not provid any arguments!');
+    }
+    message.channel.send(`Command name: ${command}\nArguments: ${args}`);
   }
 });
 
